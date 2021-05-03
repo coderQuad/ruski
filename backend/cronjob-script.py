fd559c44644d259106833286c7d8b300817fb18d
Dane Williams <dwilli36@nd.edu>
2021-05-02 22:34:59 -0400

    cronjob script complete

--- backend/cronjob-script.py
+++ backend/cronjob-script.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 '''Script for retreiving the total number of yaks and beers, then records to a json file'''
 
-import json
 import requests
+import json
+from functools import reduce
+
+def sum_yaks(a, b):
+    '''function to reduce yaks to their sum'''
+    total = b['winning_team'][0]['penalties'] + b['losing_team'][0]['penalties'] + b['winning_team'][1]['penalties'] + b['losing_team'][1]['penalties']
+    return a + total
 
 def main():
     '''main driver'''
@@ -26,9 +32,16 @@ def main():
             """
 
     games = requests.post(url, json={'query': query}).json()['data']['games']
+    yaks = reduce(sum_yaks, games, 0)
+    beers = 30 * len(games) #30 beers per game
 
-    
-    print(games)
+    result = {}
+    result['yaks'] = yaks
+    result['beers'] = beers
+
+    with open('yak-beer-totals.json', 'w') as file:
+        json.dump(result, file)
+
 
 if __name__ == '__main__':
     main()
