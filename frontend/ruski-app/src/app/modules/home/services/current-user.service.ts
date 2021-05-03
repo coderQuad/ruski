cdcce38136ecfebd924977b33a2457b8ed3c4251
Conor Murphy <conor.p.murphy52@gmail.com>
2021-05-02 21:26:48 -0400

    lol whoops

--- frontend/ruski-app/src/app/modules/home/services/current-user.service.ts
+++ frontend/ruski-app/src/app/modules/home/services/current-user.service.ts
@@ -45,6 +45,9 @@ export class CurrentUserService {
                 }
               }
             }
+            else{
+              return response;
+            }
           })
         );
       })
