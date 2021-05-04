cb40c5b485b33ee48f76bcc6574f718a1778c7ba
Billy Porter <wporter2@nd.edu>
2021-05-03 20:53:54 -0400

    Fixes registration problems

--- frontend/ruski-app/src/app/modules/auth/auth.guard.ts
+++ frontend/ruski-app/src/app/modules/auth/auth.guard.ts
@@ -29,12 +29,22 @@ export class AuthGuard implements CanActivate {
         | boolean
         | UrlTree {
         const url: string = state.url;
-        return this.hundler.checkUserExistsByEmail().pipe(
-            map((response: any) => {
-                console.log(response);
-                if (response.length > 0) {
+        // console.log(route);
+        // console.log(route);
+        // console.log(state);
+        const isRegistered = this.hundler.getStatus();
+        // console.log('here');
+        return this.auth.user$.pipe(
+            map((response: Response) => {
+                // console.log(response);
+                if (response['https://example.com/roles'][0] === 'old') {
+                    // console.log('billy');
+                    return true;
+                } else if (isRegistered) {
+                    // console.log('hundy');
                     return true;
                 }
+                this.router.navigate(['/register']);
                 return false;
             }),
             catchError((error: any) => {
