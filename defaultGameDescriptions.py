8be8790c9f284551e1dd892ae4eb38730baee749
Dane Williams <dwilli36@nd.edu>
2021-05-03 20:13:27 -0400

    changes

--- defaultGameDescriptions.py
+++ defaultGameDescriptions.py
@@ -30,8 +30,9 @@ def main():
     print(current_games)
 
     #loop through users, create a handle, and set default profile pic
+    description = ""
     for game in current_games: 
-        new_game_query = f"mutation {{ updateUser(id: \"{user['id']}\", handle: \"{handle}\", name: \"{user['name']}\", email: \"{user['email']}\", elo: {user['elo']}, friend_ids: [], profile_url: \"{default_profile_url}\"){{ id }} }}"
+        new_game_query = f"mutation {{ updateGame(id: \"{game['id']}\", location: \"fisher\", description: \"{description}\", friend_ids: []){{ id }} }}"
         # requests.post(url, json={'query': new_game_query}).json()
 
 
