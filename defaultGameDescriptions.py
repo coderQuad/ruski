3a9f230d45780afd2ebe07e2dd25c61feb710258
Dane Williams <dwilli36@nd.edu>
2021-05-03 22:16:17 -0400

    default descriptions script

--- defaultGameDescriptions.py
+++ defaultGameDescriptions.py
@@ -30,10 +30,10 @@ def main():
     print(current_games)
 
     #loop through users, create a handle, and set default profile pic
-    description = ""
+    description = "ruski is fun"
     for game in current_games: 
-        new_game_query = f"mutation {{ updateGame(id: \"{game['id']}\", location: \"fisher\", description: \"{description}\", friend_ids: []){{ id }} }}"
-        # requests.post(url, json={'query': new_game_query}).json()
+        new_game_query = f"mutation {{ updateGame(id: \"{game['id']}\", location: \"fisher\", description: \"{description}\", winning_team_player_ids: [\"{game['winning_team'][0]['id']}\", \"{game['winning_team'][1]['id']}\"], losing_team_player_ids: [\"{game['losing_team'][0]['id']}\", \"{game['losing_team'][1]['id']}\"] ){{ id }} }}"
+        requests.post(url, json={'query': new_game_query}).json()
 
 
 
